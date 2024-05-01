b97b0c05393ffceddf7631658edece8bbc07f0d8
Arsenty Streltsov <arsentystreltsov@gmail.com>
2024-05-01 16:29:37 +0300

    more

--- old files/jsFiles/homePage.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const video = document.getElementById('introVideo');
-    const mainContent = document.getElementById('mainContent'); // Убедитесь, что это добавлено
-
-    video.addEventListener('ended', function() {
-        mainContent.style.display = 'block'; // Показать основное содержимое
-        video.style.display = 'none'; // Скрыть видео
-    });
-});
