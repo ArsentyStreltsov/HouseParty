8567cd3672b999a081779e8c032e91a7dc11e449
ArsentyStreltsov <arsentystreltsov@gmail.com>
2024-05-14 19:20:14 +0200

    smooth and shine

--- htmls/jsFiles/common.js
+++ htmls/jsFiles/common.js
@@ -1,15 +1,3 @@
-function adjustGameScreen() {
-    var height = window.innerHeight; // Получаем высоту окна
-    var width = (height * 9) / 16; // Рассчитываем ширину с сохранением пропорций 9:16
-    var gameScreen = document.querySelector('.game_screen');
-    gameScreen.style.width = width + 'px'; // Устанавливаем ширину
-}
-
-// Вызываем функцию при загрузке и изменении размера окна
-window.onresize = adjustGameScreen;
-window.onload = adjustGameScreen;
-
-
 function goToHomePage() {
     sessionStorage.clear();  // Очищаем sessionStorage
     location.href = 'index.html';  // Переход на главную страницу
